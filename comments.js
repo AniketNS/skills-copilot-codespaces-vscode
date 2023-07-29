b419ab9aa3387b3c7726415efb79db0c27376ed3
Aniket Shitole <91371143+AniketNS@users.noreply.github.com>
2023-07-29 14:16:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,38 @@
-//Create a web server
+// create a web server
+// listen on port 3000
+
 const express = require('express');
-const router = express.Router();
-const commentsCtrl = require('../controllers/comments');
+const app = express();
+const port = 3000;
 
-// GET /comments
-router.get('/', commentsCtrl.index);
-// POST /comments
-router.post('/', commentsCtrl.create);
-// DELETE /comments/:id
-router.delete('/:id', commentsCtrl.delete);
+// set view engine to ejs
+app.set('view engine', 'ejs');
 
-module.exports = router;
+// set public folder to serve static files
+app.use(express.static('public'));
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', (req, res) => {
+  let drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 },
+  ];
+  let tagline =
+    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline,
+  });
+});
+
+// about page
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.listen(port);
+console.log(`Server is listening on port ${port}`);
